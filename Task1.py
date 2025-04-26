1838eff0d21e17d1f3114abfbe785358b28ae859
Ayushi Hirani <167391103+ayushihirani18@users.noreply.github.com>
2025-04-26 22:19:32 +0530

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -11,8 +11,27 @@ def encrypt(text, shift):
 
         else:
             result+= l
+            
+    return result
+
+def decrypt(cipher, shift):
+
+    result = ""
+    for l in text:
+        
+        if (l.isupper()):
+            result += chr((ord(l) - shift - 65 + 1) % 26 + 65)
+
+        elif (l.islower()):
+            result += chr((ord(l) - shift - 97 + 1) % 26 + 97)
+
+        else:
+            result+= l
+            
     return result
 
 text = input("Enter the text to encrypt:")
 shift = int(input("Enter the shift key (1-25):"))
-cipher = print(f"Encrypted text :" + encrypt(text,shift))
+cipher = encrypt(text,shift)
+print(f"Encrypted text :" + cipher)
+print(f"Decrypted text :" + decrypt(cipher,shift))
